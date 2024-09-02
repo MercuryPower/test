b92235b35bed6fda3f8433555dcdce70786dfefb
Nikita Holod <75911800+MercuryPower@users.noreply.github.com>
2024-09-02 16:30:36 +0400

    navbar menu was made

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,6 +7,9 @@ function App() {
 
   return (
     <>
+      <section>
+        <h3 className={'today-product'}>Товар дня</h3>
+      </section>
     </>
   )
 }
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -2,12 +2,59 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <header className={'navbar'}>
-            <a href={'/'}><h1 id={'navbar_company-name'}>Elfen lied</h1></a>
-            <div className={'navbar_menu-item'}>
-                <img src={'/4dots.svg'} alt={'icon'} />
-                <h2>Каталог</h2>
-            </div>
+        <header>
+            <nav className={'navbar'}>
+                <ul className={'menu'}>
+                    <li>
+                        <a href={'/'}>
+                            <h1 className={'navbar_company-name'}>Elfen lied</h1>
+                        </a>
+                    </li>
+                    <li>
+                        <a href={'/'} className={'navbar_menu-item'}>
+                            <div className={'navbar_menu-item__catalog'}>
+                                <img src={'/4dots.svg'} alt={'icon'} />
+                                <h2>Каталог</h2>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <div className={'navbar_menu-item'}>
+                            <div className={'navbar_menu-item__info'}>
+                                <a href={'/'}>
+                                    <h2>Блог</h2>
+                                </a>
+                                <a href={'/'}>
+                                    <h2>Контакты</h2>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href={'/'} className={'navbar_menu-item'}>
+                            <div className={'navbar_menu-item__search'}>
+                                <img src={'/searchIcon.svg'} alt={'searchIcon'} />
+                                <h2>Поиск</h2>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <div className={'navbar_menu-item'}>
+                            <div className={'navbar_menu-item__user-options'}>
+                                <a href={'/favorite'}>
+                                    <img src={'/favorite.svg'} alt={'icon'} />
+                                </a>
+                                <a href={'/profile'}>
+                                    <img src={'/profile.svg'} alt={'icon'} />
+                                </a>
+                                <a href={'/cart'}>
+                                    <img src={'/cart.svg'} alt={'icon'} />
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
